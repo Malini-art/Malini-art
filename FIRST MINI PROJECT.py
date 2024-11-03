9eccab833da507bfad9fb7aa60e6843b70a0d4c9
MaliniRamasamy <maliniramasamy68@gmail.com>
2024-11-03 10:40:45 +0530

    Add files via upload

---
+++ FIRST MINI PROJECT.py
@@ -0,0 +1,16 @@
+height=float(input("Enter your height in meters: "))
+weight=float(input("Enter your Weight in Kg: "))
+ 
+BMI=weight/(height*height)
+print("BMI Calculated is:  ",BMI)
+ 
+if BMI<18.5:
+    print("You are underweight")
+elif BMI==(18.5 - 24.9):
+    print("Good!You are normal")
+elif BMI==(25 - 29.9):
+    print("You are overweight")
+elif BMI>=30:
+        print("ccording to your BMI, you are classified as obese")
+else:
+    print("enter valid details")
